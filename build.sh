544132f132d3d055a1998d06172381a20de9e268
arvinnie <arvinnie@tencent.com>
2025-08-04 12:00:02 +0800

    build linux x86_64 so

--- build.sh
+++ build.sh
@@ -31,6 +31,10 @@ tree-sitter generate
 
 gcc -shared -o build/tree-sitter-objcpp.so -I./src src/parser.c src/scanner.cc -Os -fPIC -lstdc++
 
+# cross build linux output in macos
+# brew install FiloSottile/musl-cross/musl-cross
+# x86_64-linux-musl-gcc -shared -o build/tree-sitter-objcpp.so -I./src src/parser.c src/scanner.cc -Os -fPIC -lstdc++
+
 # cd ..
 # lipo -info output/tree-sitter-objcpp.so
 # lipo -create -output output/tree-sitter-objcpp.so output/tree-sitter-objcpp_arm64.so output/tree-sitter-objcpp_x86_64.so
